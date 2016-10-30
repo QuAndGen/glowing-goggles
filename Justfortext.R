a640ea07c8f1353ef36b4baf6b460996a0c40558
RamSingh1988 <ramsinghkushinagar@yahoo.co.in>
2016-10-30 11:59:44 +0530

    Add files via upload

---
+++ Justfortext.R
@@ -0,0 +1,19 @@
+setwd("D:\\Ram\\Kaggle\\Test")
+
+train<-read.csv("train.csv",header=T)
+
+
+str(train)
+
+head(train)
+train$Y<-as.factor(train$Y)
+length(train)
+rnk<-rank(x)
+library(Hmisc)
+str(x)
+x<-as.matrix(train[,-c(1,84)])
+head(x)
+a<-varclus(x, similarity=c("spearman"), 
+           type=c("data.matrix"), 
+           method="complete") 
+plot(a)
